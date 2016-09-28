7439ae5efeca8d55021a1c84b8bd41a398fb0e7c
Wilson Ding <shodai100@gmail.com>
2016-09-28 10:56:07 -0500

    Added Unit Testing and modified url option to name

---
+++ SwiftVideoBackgroundTests/SwiftVideoBackgroundTests.swift
@@ -0,0 +1,31 @@
+//
+//  SwiftVideoBackgroundTests.swift
+//  SwiftVideoBackgroundTests
+//
+//  Created by Wilson Ding on 9/28/16.
+//  Copyright © 2016 Wilson Ding. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftVideoBackground
+
+class SwiftVideoBackgroundTests: XCTestCase {
+    
+    let vc = BackgroundVideo()
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    func testAssert() {
+        // Will add tests in future
+        XCTAssert(true)
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}

f53d4c0e94bd53bdecb0966e66fcbe33822bb139
Wilson Ding <shodai100@gmail.com>
2016-09-28 09:38:28 -0500

    Added overloaded function for ability to remove alpha

--- README.md
+++ README.md
@@ -77,9 +77,14 @@ var backgroundVideo : BackgroundVideo!
 Link a UIView within a ViewController within the Storyboard to a BackgroundVideo item, or link it programmatically.
 
 ```swift
+backgroundVideo.createBackgroundVideo(url: "Background", type: "mp4")
+```
+Use the `createBackgroundVideo` function, with the name of the video or gif under `url`, and the file type (popular ones include mp4, mov, and gif) under `type`. 
+
+```swift
 backgroundVideo.createBackgroundVideo(url: "Background", type: "mp4", alpha: 0.5)
 ```
-Use the `createBackgroundVideo` function, with the name of the video or gif under `url`, the file type (popular ones include mp4, mov, and gif) under `type`, and an alpha value between 0 and 1 under `alpha`, to adjust the brightness of the video. 
+You can also include an alpha value between 0 and 1 under `alpha`, to adjust the brightness of the video.
 
 Note: Make sure you have added a video file to the project, and targeted the project. Also, make sure that you have set the module to `SwiftVideoBackground` for the BackgroundVideo UIView.
 
--- SwiftVideoBackground/BackgroundVideo.swift
+++ SwiftVideoBackground/BackgroundVideo.swift
@@ -12,13 +12,20 @@ import AVFoundation
 public class BackgroundVideo: UIView {
 
     private var player: AVPlayer?
-
-    private func createAlpha(alpha: CGFloat) {
-        let overlayView = UIView(frame: self.frame)
-        overlayView.backgroundColor = UIColor.black
-        overlayView.alpha = alpha
-        self.addSubview(overlayView)
-        self.sendSubview(toBack: overlayView)
+    
+    public func createBackgroundVideo(url: String, type: String) {
+        let path = Bundle.main.path(forResource: url, ofType: type)
+        player = AVPlayer(url: URL(fileURLWithPath: path!))
+        player!.actionAtItemEnd = AVPlayerActionAtItemEnd.none;
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.frame
+        playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        self.layer.insertSublayer(playerLayer, at: 0)
+        
+        // Set observer for when video ends and loop video infinitely
+        NotificationCenter.default.addObserver(self, selector: #selector(playerItemDidReachEnd), name: Notification.Name.AVPlayerItemDidPlayToEndTime, object: player!.currentItem)
+        player!.seek(to: kCMTimeZero)
+        player!.play()
     }
     
     public func createBackgroundVideo(url: String, type: String, alpha: CGFloat) {
@@ -36,6 +43,14 @@ public class BackgroundVideo: UIView {
         NotificationCenter.default.addObserver(self, selector: #selector(playerItemDidReachEnd), name: Notification.Name.AVPlayerItemDidPlayToEndTime, object: player!.currentItem)
         player!.seek(to: kCMTimeZero)
         player!.play()
+    }
+    
+    private func createAlpha(alpha: CGFloat) {
+        let overlayView = UIView(frame: self.frame)
+        overlayView.backgroundColor = UIColor.black
+        overlayView.alpha = alpha
+        self.addSubview(overlayView)
+        self.sendSubview(toBack: overlayView)
     }
     
     @objc private func playerItemDidReachEnd() {

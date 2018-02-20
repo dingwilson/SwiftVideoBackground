294f7cc657ef1c878e8cdcb43482603653242d4f
Wilson Ding <hello@wilsonding.com>
2018-02-20 14:01:25 -0600

    Added a few basic tests

--- SwiftVideoBackgroundTests/SwiftVideoBackgroundTests.swift
+++ SwiftVideoBackgroundTests/SwiftVideoBackgroundTests.swift
@@ -5,10 +5,33 @@
 //  Created by Wilson Ding on 9/28/16.
 //  Copyright © 2016 Wilson Ding. All rights reserved.
 //
+
 import XCTest
 import SwiftVideoBackground
 
 class SwiftVideoBackgroundTests: XCTestCase {
+    func testDidCreateVideoInfo() {
+        let videoInfo = VideoInfo(name: "Background", type: "mp4")
+
+        XCTAssertEqual(videoInfo.name, "Background")
+        XCTAssertEqual(videoInfo.type, "mp4")
+    }
+
+    func testDidSuccessfullyCreateBackgroundVideo() {
+        let view = UIView()
+
+        do {
+            try VideoBackground.shared.play(view: view,
+                                            name: "Background",
+                                            type: "mp4",
+                                            isMuted: true,
+                                            alpha: 0.2,
+                                            willLoopVideo: true)
+        } catch {
+            XCTAssertNil(error)
+        }
+    }
+
     func testDidCatchNonexistantVideo() {
         let view = UIView()
         let videoInfo = VideoInfo(name: "NonExistantVideo", type: "mp4")
